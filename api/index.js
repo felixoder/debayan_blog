75401d793df14f39b3be6c275803117a5d853447
Debayan <debayanghosh408@gmail.com>
2024-01-28 01:08:28 +0530

    The Database is perfectly set up

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("The database is connected");
+  })
+  .catch((err)=>{
+    console.log('Error');
+  })
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-})
