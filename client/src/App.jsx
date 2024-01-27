61e4e91d7d7a3aed8a1b23ab35a5b77db50b5e64
Debayan <debayanghosh408@gmail.com>
2024-01-27 23:33:22 +0530

    create the pages and the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/Signin'
+import SignUp from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>
-      This is debayan ghosh and this is the mern blogg
-    </h1>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/'  element={<Home/>}/>
+        <Route path='/about'  element={<About/>}/>
+        <Route path='/sign-in'  element={<SignIn/>}/>
+        <Route path='/sign-up'  element={<SignUp/>}/>
+        <Route path='/dashboard'  element={<Dashboard/>}/>
+        <Route path='/projects'  element={<Projects/>}/>
+
+
+        </Routes>
+    </BrowserRouter>
   )
 }
