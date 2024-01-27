04d02c782072a8858031822b7c979276d1454dbc
Debayan <debayanghosh408@gmail.com>
2024-01-27 23:08:02 +0530

    install react js and tailwind css and create the template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-3xl text-red-500'>
+      This is debayan ghosh and this is the mern blogg
+    </h1>
+  )
+}
