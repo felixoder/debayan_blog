bdb5c04583c154f50925938c6bab538b205a51fa
Debayan <debayanghosh408@gmail.com>
2024-01-29 21:28:20 +0530

    created the signup api routes properly

--- api/index.js
+++ api/index.js
@@ -1,23 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
+const app = express();
+
+// Use express.json() middleware
+app.use(express.json());
+
 mongoose
-  .connect(
-    process.env.MONGO
-  )
+  .connect(process.env.MONGO ||'mongodb+srv://felix:fgIV5oOtf5iWsRpz@mern-blog.wjyv65z.mongodb.net/mern-blog?retryWrites=true&w=majority')
   .then(() => {
     console.log("The database is connected");
   })
-  .catch((err)=>{
-    console.log('Error');
-  })
-
-const app = express();
+  .catch((err) => {
+    console.error("Error connecting to the database:", err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
