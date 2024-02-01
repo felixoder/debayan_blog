c40d7b26b894ec70a934073f5d5dd3061fbc68af
Debayan <debayanghosh408@gmail.com>
2024-02-01 21:27:52 +0530

    completed the sidebar and updated the profile

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom';
+
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('')
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl){
+      setTab(tabFromUrl)
+    }
+
+  },[location.search])
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className="mid:w-56">
+
+        {/* sidebar */}
+        <DashSidebar/>
+      </div>
+      {/* profile.... */}
+      {tab==='profile' &&<DashProfile/>}
+      
     </div>
   )
 }
