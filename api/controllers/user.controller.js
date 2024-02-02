e17deff6329eb195e3a27be3b5e402c8500dd28b
Debayan <debayanghosh408@gmail.com>
2024-02-03 02:58:25 +0530

    complete update user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,47 +8,48 @@ export const test = (req, res) => {
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandeler(403, "You are not allowed to update this user"));
+    return next(errorHandler(403, 'You are not allowed to update this user'));
   }
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandeler(400, "Password must be atleast 6 characters"));
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandeler(400, "Username must be between 7 and 20 characters")
+        errorHandler(400, 'Username must be between 7 and 20 characters')
       );
     }
-    if (req.body.username.includes(" ")) {
-      return next(errorHandeler(400, "Username cannot contain spaces"));
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandeler(400, "Username must be lowercase"));
+      return next(errorHandler(400, 'Username must be lowercase'));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandeler(400, "Username can only contains letters and numbers")
+        errorHandler(400, 'Username can only contain letters and numbers')
       );
     }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-      
-        { new: true }  );
-        const {password, ...rest} = updateUser._doc;
-    } catch (error) {
-      next(error);
-    }
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use((err,req,res,next)=>{
   const statusCode = err.statusCode ||500
-  const message = err.message || 'Internal server error'
+  const message = err.message
   res.status(statusCode).json({
     success: false,
     statusCode,
