d8b85b007ae69e59b5cad45dd150aca4e5303a23
Debayan <debayanghosh408@gmail.com>
2024-02-04 02:25:28 +0530

    create the post api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js'
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -38,3 +39,4 @@ app.use((err,req,res,next)=>{
   })
 
 })
+app.use('/api/post' , postRoutes)
