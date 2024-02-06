af2076e1fb9305a4fe2868dfe8618953c626631a
Debayan <debayanghosh408@gmail.com>
2024-02-07 02:19:37 +0530

    add deleteuser functionality to the user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,10 +4,11 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 router.get('/test',test);
-router.put('/update/:userId',verifyToken,  updateUser)
-router.delete('/delete/:userId',verifyToken, deleteUser)
+router.put('/update/:userId',verifyToken,  updateUser);
+router.delete('/delete/:userId',verifyToken, deleteUser);
 router.post('/signout',signout);
-router.get('/getusers' , verifyToken , getUsers)
+router.get('/getusers' , verifyToken , getUsers);
+
 
 
 export default router
