69fe212850ec10b7fe70908c77912d2d6efd0b3a
Debayan <debayanghosh408@gmail.com>
2024-02-07 21:35:38 +0530

    Add Cal To action to the post page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import {BsFacebook , BsInstagram , BsTwitterX , BsGithub , BsLinkedin} from 'rea
 
 export default function FooterCom() {
   return (
-    <Footer container className="border border-t-8 border-teal-500">
+    <Footer container className="border border-t-4 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
