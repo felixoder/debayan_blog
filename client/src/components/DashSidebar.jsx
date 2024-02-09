d94e7593e302a17d758293ab2694b1d7b4f894c0
Debayan <debayanghosh408@gmail.com>
2024-02-09 21:26:13 +0530

    add comment section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiDocumentText, HiOutlineUser, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
+import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUser, HiOutlineUserGroup, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { signOutSuccess } from '../redux/user/userSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -51,11 +51,19 @@ export default function DashSidebar() {
               </Sidebar.Item>
             </Link>
           )} {currentUser.isAdmin && (
-            <Link to='/dashboard?tab=users'>
+            <>
+               <Link to='/dashboard?tab=users'>
               <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
                 Users
               </Sidebar.Item>
             </Link>
+            <Link to='/dashboard?tab=comments'>
+              <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+                Comments
+              </Sidebar.Item>
+            </Link>
+            </>
+         
           )}
           <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignOut}>
             Sign-Out
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashSidebar from '../components/DashSidebar';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -30,6 +31,8 @@ export default function Dashboard() {
       {tab === 'posts' && <DashPosts/>}
       {/* users */}
       {tab === 'users' && <DashUsers/>}
+      {/* comments */}
+      {tab === 'comments' && <DashComments/>}
       
     </div>
   )
