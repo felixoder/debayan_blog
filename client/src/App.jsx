8159b66016f8d9c16c36409fd146835ac7c8768a
Debayan <debayanghosh408@gmail.com>
2024-02-09 22:39:05 +0530

    fix the vulnarable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 
-import SignUp from './pages/Signup'
+import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header.jsX'
