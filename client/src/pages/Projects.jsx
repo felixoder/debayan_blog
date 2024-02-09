d73399fa639405854ab51c25e88839039ae47d18
Debayan <debayanghosh408@gmail.com>
2024-02-09 22:19:07 +0530

    added the projects and about pages to webpage

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-md text-gray-500'>Coming Soon.... Stay Tuned</p>
+      <CallToAction />
     </div>
   )
 }
