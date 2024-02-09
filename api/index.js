31a938d7fef3257610e34e8cb63e8d3462aabc26
Debayan <debayanghosh408@gmail.com>
2024-02-09 22:28:56 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -6,15 +6,17 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
+
 
 dotenv.config();
 
 const app = express();
 
-// Use express.json() middleware
+
 app.use(express.json());
 app.use(cookieParser());
-
+const __dirname = path.resolve();
 mongoose
   .connect(process.env.MONGO ||'mongodb+srv://felix:fgIV5oOtf5iWsRpz@mern-blog.wjyv65z.mongodb.net/mern-blog?retryWrites=true&w=majority')
   .then(() => {
@@ -30,7 +32,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
-app.use('/api/comment',commentRoutes)
+app.use('/api/post',postRoutes)
+app.use('/api/comment',commentRoutes);
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname, 'client','dist','index.html'))
+})
 app.use((err,req,res,next)=>{
   const statusCode = err.statusCode ||500
   const message = err.message
