c47eb4a68278107be9bf917f38645082059cf4e5
Debayan <debayanghosh408@gmail.com>
2024-02-09 21:05:37 +0530

    add recent articles sections to the post pages

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [ require('flowbite/plugin'), require('tailwind-scrollbar'),],
-}
+  plugins: [
+    require("flowbite/plugin"),
+    require("tailwind-scrollbar"),
+    
+  ],
+};
